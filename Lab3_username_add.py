d4d5a4f5e95e5b0e60868fb3172cc65f4a106a7f
Rajani <rajani.kami004@gmail.com>
2026-02-05 18:24:30 -0500

    Appending 5 item to Items list

--- Lab3_username_add.py
+++ Lab3_username_add.py
@@ -4,3 +4,8 @@ Purpose:Adding to list
 Starter code: None
 Date:02/05/2026"""
 from Lab3_rabarail_list import Items
+Items.append("First aid kit")
+Items.append("Insect repellent")
+Items.append("Portable fan")
+Items.append("Camera")
+Items.append("Jacket")
