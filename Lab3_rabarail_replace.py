f8601d95875ef4c534f83c11757e578b21b3a652
Rajani <rajani.kami004@gmail.com>
2026-02-05 18:42:50 -0500

    Replacing item with: Binocular

---
+++ Lab3_rabarail_replace.py
@@ -0,0 +1,8 @@
+"""Program name: Working with lists 
+AUthor: Rajani Baraili
+Purpose:Replacing items in list
+Starter code: None
+Date:02/05/2026"""
+from Lab3_Rabarail_add import Items
+Items[4] = "Binoculars"
+      
