ee17929052486d087d300975f9ce704ae5ac6734
Rajani <rajani.kami004@gmail.com>
2026-02-05 15:23:37 -0500

    Add Items in string

--- Lab3_rabarail_list.py
+++ Lab3_rabarail_list.py
@@ -3,4 +3,15 @@ AUthor: Rajani Baraili
 Purpose:Creating and sorting list 
 Starter code: None
 Date:02/05/2026"""
-
+Items ==[
+    "Tent poles",
+    "Suncreen",
+    "Snacks",
+    "Water bottle",
+    "Sleeping bag",
+    "Hat",
+    "Portable charger",
+    "Flashlight",
+    "Sunglasses",
+    "Map"
+]
