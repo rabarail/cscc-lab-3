0333996c6c0363857ed02fdea3c3635a5e896a0f
Rajani <rajani.kami004@gmail.com>
2026-02-05 19:13:16 -0500

    Printing total number of items bought to camping trip

--- Lab3_rabarail_remove.py
+++ Lab3_rabarail_remove.py
@@ -5,5 +5,4 @@ Starter code: None
 Date:02/05/2026"""
 from Lab3_rabarail_replace import Items
 Items.remove("Binoculars")
-print("Items after removing binoculars:")  
-print(Items)
+print(f"Total number of items in the list: {len(Items)}")  
